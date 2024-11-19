b8e00679761d8d6883319b7a7a5c987639db9d9a
Stefan Sjöberg <bird.nerd@outlook.com>
2024-11-19 11:46:30 +0100

    Add project files.

---
+++ Villkorsstyrning/Program.cs
@@ -0,0 +1,21 @@
+﻿int minValue = 0;
+int maxValue = 100;
+
+int currentValue = 100;
+
+if (currentValue <= minValue)
+{
+    Console.WriteLine("Värdet är mindre eller lika med minvärdet");
+}
+else if (currentValue > maxValue)
+{
+    Console.WriteLine("Värdet är större än minvärdet");
+}
+else
+{
+    Console.WriteLine("Värdet håller sig inom sina ramar");
+}
+
+Console.ReadKey();
+
+/* */
